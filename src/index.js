910120c955dab2dfa5b78f3b16fef3cb6b335b81
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2020-10-04 17:59:57 +0300

    feat: implement decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openedStack = [];
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+        for (let i = 0; i < str.length; i++) {
+
+            for (let item of bracketsConfig) {
+
+                if (str[i] == item[1] && openedStack[openedStack.length - 1] == item[0]) {
+                    openedStack.pop();
+                }
+
+                else if (str[i] == item[0]) {
+                    openedStack.push(str[i]);
+                }
+
+            }
+            
+        }
+    
+    if (openedStack.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  
 }
+
+
